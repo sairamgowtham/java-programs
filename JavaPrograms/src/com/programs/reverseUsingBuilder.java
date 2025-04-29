607fea0a635cb629cb4ed3076e92085b943a4970
sairamgowtham <saigowtham1290@gmail.com>
2025-04-29 22:28:32 +0530

    second commit

--- JavaPrograms/src/com/programs/reverseUsingBuilder.java
+++ JavaPrograms/src/com/programs/reverseUsingBuilder.java
@@ -9,7 +9,8 @@ public class reverseUsingBuilder {
 		Scanner scan = new Scanner(System.in);
 		String input = scan.nextLine();
 		StringBuilder sb = new StringBuilder(input);
-		System.out.println(sb.reverse());
+		System.out.println("The reverse word is"+sb.reverse());
+
 
 		
 	}
