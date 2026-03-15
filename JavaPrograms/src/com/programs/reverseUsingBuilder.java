0685f3d6dea24623f0f428fe7382e1e65e665dee
sairamgowtham <saigowtham1290@gmail.com>
2026-03-15 18:29:26 +0530

    updated reverse program

--- JavaPrograms/src/com/programs/reverseUsingBuilder.java
+++ JavaPrograms/src/com/programs/reverseUsingBuilder.java
@@ -9,8 +9,13 @@ public class reverseUsingBuilder {
 		Scanner scan = new Scanner(System.in);
 		String input = scan.nextLine();
 		StringBuilder sb = new StringBuilder(input);
-		System.out.println("The reverse word is"+sb.reverse());
-
+		System.out.println("The reverse word is"+);
+        String reverse = sb.reverse().toString();
+		if(input.equals(reverse)) {
+			System.out.println("The word is palindrome");
+		} else {
+			System.out.println("The word is not palindrome");
+		}
 
 		
 	}
